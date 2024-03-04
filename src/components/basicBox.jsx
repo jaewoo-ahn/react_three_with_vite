1d46234acb8c4936a9d0b2e31f4072fefb4f13f6
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-03-04 19:24:29 +0900

    four dimension

--- src/components/basicBox.jsx
+++ src/components/basicBox.jsx
@@ -8,6 +8,18 @@ const BasicBox = () => {
         <boxGeometry />
         <meshStandardMaterial color={"orange"} />
       </mesh>
+      <mesh position={[-1, 0, 0]}>
+        <boxGeometry />
+        <meshStandardMaterial color={"orange"} />
+      </mesh>
+      <mesh position={[-1, 2, 0]}>
+        <boxGeometry />
+        <meshStandardMaterial color={"orange"} />
+      </mesh>
+      <mesh position={[1, 2, 0]}>
+        <boxGeometry />
+        <meshStandardMaterial color={"orange"} />
+      </mesh>
     </Canvas>
   );
 };
